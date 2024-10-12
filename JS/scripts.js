917fcdabd38ee4c0e99060273ce61092b754a10d
Aurélien PAILLARD <aurelien.paillard@edu.univ-fcomte.fr>
2024-10-12 12:28:08 +0200

    Added popups for all game cards

---
+++ JS/scripts.js
@@ -0,0 +1,51 @@
+function plageRequiem() {
+    Swal.fire({
+        title: "A Plage Tale : Requiem",
+        icon : 'info',
+        showCloseButton : true,
+        html :
+            '<h2 style="font-size: 20pt">Fiche technique :</h2>' +
+            '<span><strong>Genre :</strong> Action Aventure</span><br>' +
+            '<span><strong>Editeur :</strong> Focus Home Interactive</span><br><br>' +
+            '<p>Embarquez dans la suite bouleversante de A Plague Tale: Innocence, le jeu aux multiples récompenses ! Aux côtés d’Amicia et Hugo, surmontez tous les dangers et vivez une nouvelle aventure à couper le souffle dans un monde brutal et cruel.</p>'
+    })
+}
+
+function flightSim() {
+    Swal.fire({
+        title: "Microsoft Flight Simulator",
+        icon : 'info',
+        showCloseButton : true,
+        html :
+            '<h2 style="font-size: 20pt">Fiche technique :</h2>' +
+            '<span><strong>Genre :</strong> Simulation</span><br>' +
+            '<span><strong>Editeur :</strong> Microsoft</span><br><br>' +
+            '<p>Avion de tourisme ou jets puissants, volez avec les avions les plus détaillés et fidèles dans un monde incroyablement réaliste.</p>'
+    })
+}
+
+function plagueInno() {
+    Swal.fire({
+        title: "A Plague Tale: Innocence",
+        icon : 'info',
+        showCloseButton : true,
+        html :
+            '<h2 style="font-size: 20pt">Fiche technique :</h2>' +
+            '<span><strong>Genre :</strong> Action Aventure</span><br>' +
+            '<span><strong>Editeur :</strong> Focus Home Interactive</span><br><br>' +
+            '<p>1348. Le fléau de la peste ravage le Royaume de France. A travers les villages dévastés par la maladie, Amicia et son jeune frère Hugo tentent de fuir l’inquisition lancée à leur poursuite. Sur leur route, ils devront joindre leurs forces à celles d’autres orphelins et survivre à des hordes de rats, par le feu et la lumière. Grâce au lien qui les unit, les enfants affronteront les jours les plus sombres de l’Histoire pour échapper au destin funeste de leur famille.</p>'
+    })
+}
+
+function theCrew() {
+    Swal.fire({
+        title: "The Crew 2",
+        icon : 'info',
+        showCloseButton : true,
+        html :
+            '<h2 style="font-size: 20pt">Fiche technique :</h2>' +
+            '<span><strong>Genre :</strong> Conduite en Monde Ouvert</span><br>' +
+            '<span><strong>Editeur :</strong> Ubisoft</span><br><br>' +
+            '<p>Explorez librement un pays tout entier par terre, mer et airs ! D’une côte à l’autre, des pilotes street et pro, des fans de hors-piste et des freestylers se réunissent pour s’affronter au cours d’épreuves variées. Participez à des courses effrénées et partagez chacun de vos instants de gloire avec le reste du monde.</p>'
+    })
+}
